429e073f1899cd43039be2c80fb8d1777b8eb8fb
Anissa7 <a.ouedraogo@alustudent.com>
2023-02-23 04:53:43 -0800

    les choses de la vie

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/user/bin/python3
+from add_0 import add
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} - {} = {}".format(a, b, sub(a, b)))    
