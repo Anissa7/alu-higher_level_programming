b67fba9104f36df0262885e593b7fd8178e26242
Anissa7 <a.ouedraogo@alustudent.com>
2023-03-10 12:20:06 -0800

    Let us see

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 f __name__ == "__main__":
         """Number of argument."""
-            import sys:wq
-
+            import sys
                 count = len(sys.argv) - 1
                     if count == 0:
                                 print("0 arguments.")
