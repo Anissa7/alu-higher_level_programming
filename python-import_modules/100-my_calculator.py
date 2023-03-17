52cc943f75cad147be5e113b3db41675ba731463
Anissa7 <a.ouedraogo@alustudent.com>
2023-03-17 04:07:42 -0700

    Advanced 100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -12,7 +12,13 @@ if __name__ == "__main__":
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
         else:
-            a = int(sys.argv[1])                                                                  b = int(sys.argv[3])                                                                 if sys.argv[2] == "+":
-               print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))                               elif sys.argv[2] == "-":
-               print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))                               elif sys.argv[2] == "*":                                                                  print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-            elif sys.argv[2] == "/":                                                                 print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+            a = int(sys.argv[1])                                                                
+            b = int(sys.argv[3])                                                                  
+            if sys.argv[2] == "+":
+               print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))                          
+            elif sys.argv[2] == "-":
+               print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))                               
+           elif sys.argv[2] == "*":                                                                  
+               print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+            elif sys.argv[2] == "/":                                                                
+                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
