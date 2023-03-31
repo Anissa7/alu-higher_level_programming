7650955b37471c288c40a6f5dc0761aff5a41b2a
Anissa7 <a.ouedraogo@alustudent.com>
2023-03-31 06:58:23 -0700

    from task 5 to task 16

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- script that removes  the database hbtn_0c_0 in your MySQL server.
+ DROP database  IF NOT EXISTS hbtn_0c_0;
+
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays number of records with id = 89 in first_table of hbtn_0c_0 in MySQL server
+SELECT count(*) FROM first_table WHERE `id` = 89;
+
