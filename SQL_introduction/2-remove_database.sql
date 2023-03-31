4fab83ca55366926a21079b44ff191d379c5ad26
Anissa7 <a.ouedraogo@alustudent.com>
2023-03-31 07:08:20 -0700

    task 2 correction

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,4 @@
--- Deletes  the database hbtn_0c_0 in MySQL server.
- DROP database  IF NOT EXISTS hbtn_0c_0;
+-- Deletes the database hbtn_0c_0 in MySQL server.
+DROP database IF EXISTS hbtn_0c_0;
+
 
