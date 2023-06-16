3516184403a4041cbb8fbcebd51ce012aa95f15e
Anissa7 <useradd@DESKTOP-B5A5SLD>
2023-06-16 22:40:25 +0200

    the repos is correct

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-module.exports = class Rectangle {};
+class Rectangle {
+    constructor (w, h) {
+      if ((w = parseInt(w)) > 0 && (h = parseInt(h)) > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
+  module.exports = Rectangle;
